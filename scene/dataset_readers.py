e74ce42ff71e2856fb831d33215105f32d206238
YifanWang00 <yifanwang.xyz@gmail.com>
2024-08-21 04:53:34 +0000

    Add more logger for mapping

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -1090,9 +1090,9 @@ def readAVLSceneInfo(
             poses.append(pose)
         return poses
 
-    color_path = "resolution_640x480/color_mask_sky-car-human-road-tree_cut"
-    depth_path = "depth/zoedepth/with_mask/sky-car-human-road-tree_cut"
-    pose_path = "cam_pose_cut"
+    color_path = "resolution_640x480/color_mask_sky-car-human-road_cut_2"
+    depth_path = "depth/zoedepth/with_mask/sky-car-human-road_cut_2"
+    pose_path = "cam_pose_cut_2"
     if eval_:
         color_path += "_eval"
         depth_path += "_eval"
